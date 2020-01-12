8b98bcddf31bdf75291a8d06af1288ab2f25db3f
gnanasurya <gnanasurya2@gmail.com>
2020-01-12 16:19:41 +0530

    anxiety

--- anxiety.js
+++ anxiety.js
@@ -21,3 +21,39 @@ function onclik() {
         }, 200);
     }
 }
+
+var txt3 = "Remember that you are not alone !";
+var k = 0;
+var speed = 50;
+
+function addSymbols3() {
+    document.getElementById("title3").innerHTML = document
+        .getElementById("title3")
+        .innerHTML.substr(
+            0,
+            document.getElementById("title3").innerHTML.length - 38
+        );
+}
+
+function typeWriter3() {
+    var symbols = ["*", "-", "$", "%", "?", "@"];
+    if (k < txt3.length) {
+        var sym = symbols[Math.floor(Math.random() * 5)];
+        if (txt3.charAt(k) == "!" || (txt3.charAt(k) == "u" && k > 3)) {
+            document.getElementById("title3").innerHTML +=
+                '<span style="color: #eb4034;">' + txt3.charAt(k) + "</span>" + sym;
+        } else {
+            document.getElementById("title3").innerHTML += txt3.charAt(k) + sym;
+        }
+
+        document.getElementById("title3").innerHTML = document
+            .getElementById("title3")
+            .innerHTML.replace(
+                sym,
+                '<span style="color: #eb4034;">' + sym + "</span>"
+            );
+        k++;
+        setTimeout(addSymbols3, speed);
+        setTimeout(typeWriter3, speed);
+    }
+}
