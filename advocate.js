ef41b1ea3971f3e3b009c7949100733343f8c8bd
gnanasurya <gnanasurya2@gmail.com>
2020-01-14 15:12:54 +0530

    added youth advocates

--- advocate.js
+++ advocate.js
@@ -1,14 +1,16 @@
 function advocatesLoader() {
     console.log("hello");
-    for (var i = 0; i < 39; i++) {
+    var yaName = ['Aruna choudhary ', 'Sowmya', 'Yashoda', 'Mohan', 'Ch. Ajit', 'Thippani manideep', 'Sai ', 'J.G.Sushma ', 'MANDRU SANJAY KUMAR', 'KARTHICK', 'MUMMANENI SAI HARSHA', 'Akanksha Mudgal ', 'Savudam Sai Sathvik', 'CHANDAN', 'Aishwarya', 'HARSHA', 'SUDHARSHAN', 'Chakri', 'Sudipa', 'Irfan', 'Ranadheer ', 'Sushmita', 'Sai kumar', 'Harsha', 'Anil', 'Akanshya ', 'Satya datta jupalli']
+    var college = ['MNIT ,Allahabad ', 'IIT BHILAI', 'NIT Warangal', 'Vignan', 'Gitam college Vizag', 'Maulana azad national Institute of technology', 'IIT VARANASI', 'NIT Kurukshetra ', 'NIT Durgapur', 'IIT Roorkee', 'IIT Dharwad', 'NIT ARUNACHAL PRADESH ', 'IIT Gandhinagar', 'IIIT kota', 'Andhra University', 'IIT DHANBAD', 'NIT PATNA', 'IIT Bhubaneswar', 'Ravenshaw University', 'Gurunanak Institutions', 'IIT Bombay ', "Andhra University women's ", 'IIT VARANASI', 'NIT Jamshedpur ', 'LPU', 'CET , Bhubaneshwar ', 'VITAP']
+    for (var i = 0; i < yaName.length; i++) {
         var container = document.createElement("div");
         var image = document.createElement('img');
-        image.setAttribute('src', './8983.jpg');
+        image.setAttribute('src', './ya/ya' + (i + 1) + '.jpeg');
         image.classList.add('advocate_image');
         container.classList.add('container');
         var advocate_text = document.createElement("p");
         advocate_text.classList.add('advocate_text');
-        advocate_text.innerHTML = "Gnana surya" + "<br>" + "NIT Agartala";
+        advocate_text.innerHTML = yaName[i] + "<br>" + college[i];
         container.appendChild(image);
         container.appendChild(advocate_text);
         document.getElementById('container_box').appendChild(container);
